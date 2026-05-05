29712280ce33095b58cb017b4d39cdee56c71159
Defaulti0 <crismartin994@gmail.com>
2026-05-04 23:01:03 -0400

    Updated placeholder for progress component

--- src/Pages/Progress/Progress.js
+++ src/Pages/Progress/Progress.js
@@ -2,12 +2,19 @@ import "../../index.css";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 
+// Get the current user's user ID, compare that with the "User Progress" documents
+//  to make sure we're updating the correct user's progress
+
+
 export default function Progress() {
   return (
     <div>
       <Container className="homeContainer">
         <Row style={{ margin: "2rem" }} className="text-center">
           <h1 style={{ marginBottom: "2rem" }}>Progress</h1>
+        </Row>
+        <Row>
+            <p>Progress per location will go here</p>
         </Row>
       </Container>
     </div>
